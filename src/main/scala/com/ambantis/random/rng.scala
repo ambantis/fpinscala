a69483794c5b1eec4db2d369795f62675eb4f149
Alexandros Bantis <ambantis@gmail.com>
2015-06-04 18:21:13 -0700

    through 6.3

--- src/main/scala/com/ambantis/random/rng.scala
+++ src/main/scala/com/ambantis/random/rng.scala
@@ -3,7 +3,52 @@ package com.ambantis.random
 import scala.annotation.tailrec
 
 trait RNG {
+  import RNG._
+
+  type Rand[+A] = RNG => (A, RNG)
+
   def nextInt: (Int, RNG)
+
+  val int: Rand[Int] = _.nextInt
+
+  val double: Rand[Double] = map(int)(_ / Int.MaxValue.toDouble)
+
+  val randIntDouble: Rand[(Int, Double)] = both(int, double)
+
+  val randDoubleInt: Rand[(Double, Int)] = both(double, int)
+
+  def unit[A](a: A): Rand[A] = rng => (a, rng)
+
+  def map[A,B](s: Rand[A])(f: A => B): Rand[B] = rng => {
+    val (a, rng2) = s(rng)
+    f(a) -> rng2
+  }
+
+  def ints(n: Int): Rand[List[Int]] = sequence(List.fill(n)(int))
+
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = rng => {
+    val (a, rng1) = f(rng)
+    g(a)(rng1)
+  }
+
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A,B) => C): Rand[C] = {
+    flatMap(ra) { a =>
+      map(rb) { b => f(a,b)
+      }
+    }
+  }
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
+    fs.foldRight(unit(List.empty[A])) { (b, a) =>
+      map(both(b,a))(tup => tup._1 :: tup._2)
+    }
+
+  }
+
+  def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] = map2(ra, rb)((_, _))
+
+  def nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i => i - i % 2)
+
 }
 
 object RNG {
@@ -18,7 +63,6 @@ object RNG {
     if (i2 > 1) (i2, rng2)
     else -1 * (i2 + 1) -> rng2
   }
-
   def double(rng: RNG): (Double, RNG) = {
     val (i, rng1) = rng.nextInt
     i / Int.MaxValue.toDouble -> rng1
